bab52157fcf9acdf534f84c172f71c6bfc34736c
Ibrahim5 <104748462+mdibrahim5@users.noreply.github.com>
2024-06-27 16:18:45 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+jQuery (document).ready(function() {
+   jQuery(".owl-carousel").owlCarousel({
+       'items' : 4,
+       'autoPlay' : 2000,
+       'goToFirst': true,
+       'goToFirstSpeed': 1000,
+   });
+});
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
